4f8332b529523835d45fba3aed4ddaea5c978981
rcadaoas <romeo.mail7@gmail.com>
2014-08-01 14:06:28 +0400

    38 - Making Sweet Custom Shapes!

--- Buckys Tutorial - HTML5/bucky-canvas.js
+++ Buckys Tutorial - HTML5/bucky-canvas.js
@@ -1,14 +1,25 @@
 
 function doFirst(){
-	// 37 - Gradients on the Canvas
+	// 38 - Making Sweet Custom Shapes!
 	var x = document.getElementById('canvas');
 	canvas = x.getContext('2d'); //without var means its a global variable
-	var g = canvas.createLinearGradient(0,0,100,100);
-	g.addColorStop(.0,"blue");
-	g.addColorStop(.5,"green");
-	g.addColorStop(1,"red");
-	canvas.fillStyle=g;
-	canvas.fillRect(0,0,100,100);
+	canvas.beginPath();
+	canvas.moveTo(50,50);
+	canvas.lineTo(70,250);
+	canvas.lineTo(300,200);
+	canvas.closePath();
+	canvas.stroke();
+	
+	// 37 - Gradients on the Canvas
+	// var x = document.getElementById('canvas');
+	// canvas = x.getContext('2d'); //without var means its a global variable
+	// var g = canvas.createLinearGradient(0,0,100,100);
+	// g.addColorStop(.0,"blue");
+	// g.addColorStop(.5,"green");
+	// g.addColorStop(1,"red");
+	// canvas.fillStyle=g;
+	// canvas.fillRect(0,0,100,100);
+	
 	// 36 - Drawing Shapes on the Canvas
 	// var x = document.getElementById('canvas');
 	// canvas = x.getContext('2d'); //without var means its a global variable
