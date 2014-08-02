ec0ea2a6f4e26576bafa3d541b87bd44cf1987cb
rcadaoas <romeo.mail7@gmail.com>
2014-08-02 15:32:54 +0400

    47 - Spice it Up

--- Buckys Tutorial - HTML5/bucky-draganddrop.js
+++ Buckys Tutorial - HTML5/bucky-draganddrop.js
@@ -3,10 +3,13 @@ window.onload=doFirst;
 function doFirst(){
 	mypic = document.getElementById('facepic');
 	mypic.addEventListener("dragstart", startDrag, false);
+	mypic.addEventListener("dragend", endDrag, false);
 	leftbox = document.getElementById('leftbox');
-	leftbox.addEventListener("dragenter",function(e){e.preventDefault();} ,false);
+	leftbox.addEventListener("dragenter",dragenter ,false);
+	leftbox.addEventListener("dragleave", dragleave,false);
 	leftbox.addEventListener("dragover",function(e){e.preventDefault();} ,false);
 	leftbox.addEventListener("drop", dropped,false);
+
 }
 
 function startDrag(e){
@@ -18,4 +21,22 @@ function startDrag(e){
 function dropped(e){
 	e.preventDefault();
 	leftbox.innerHTML=e.dataTransfer.getData('Text');	
+}
+
+function endDrag(e){
+	pic = e.target;
+	pic.style.visibility='hidden';
+
+}
+
+function dragenter(e){
+	e.preventDefault();
+	leftbox.style.background="SkyBlue";
+	leftbox.style.border="3px solid red";
+}
+
+function dragleave(e){
+	e.preventDefault();
+	leftbox.style.background="white";
+	leftbox.style.border="3px solid blue";
 }
