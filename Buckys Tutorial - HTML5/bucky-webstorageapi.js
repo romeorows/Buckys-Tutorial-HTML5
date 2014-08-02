9d85d63c29fb216452e7c576d4458fd3b9ec540c
rcadaoas <romeo.mail7@gmail.com>
2014-08-02 16:18:36 +0400

    52 - Making Something Useful

--- Buckys Tutorial - HTML5/bucky-webstorageapi.js
+++ Buckys Tutorial - HTML5/bucky-webstorageapi.js
@@ -3,6 +3,8 @@ window.addEventListener("load",doFirst,false);
 function doFirst(){
 	var button = document.getElementById('button');
 	button.addEventListener("click",saveCrap,false);
+
+	display();
 }
 
 function saveCrap(){
@@ -10,13 +12,20 @@ function saveCrap(){
 	var two = document.getElementById('two').value;
 	sessionStorage.setItem(one,two);
 
-	display(one);
-
+	display();
+	document.getElementById('one').value="";
+	document.getElementById('two').value="";
 }
 
-function display(one){
+function display(){
 	var rightbox= document.getElementById('rightbox');
-	var two = sessionStorage.getItem(one);
+	
+	rightbox.innerHTML="";
 
-	rightbox.innerHTML="Name of variable "+one+"<br />Value: "+two;
+	for (var x = 0; x < sessionStorage.length; x++) {
+		
+	}
+	//51 - Retrieving and Displaying Stored Data
+	// var two = sessionStorage.getItem(one);
+	// rightbox.innerHTML="Name of variable: "+one+"<br />Value: "+two;
 }
