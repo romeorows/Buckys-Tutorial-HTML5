a2a76dfbbd2de96b797174edc411111c71025ec4
rcadaoas <romeo.mail7@gmail.com>
2014-08-02 15:23:24 +0400

    46 - Finishing the Drag and Drop Program!

--- Buckys Tutorial - HTML5/bucky-draganddrop.js
+++ Buckys Tutorial - HTML5/bucky-draganddrop.js
@@ -8,3 +8,14 @@ function doFirst(){
 	leftbox.addEventListener("dragover",function(e){e.preventDefault();} ,false);
 	leftbox.addEventListener("drop", dropped,false);
 }
+
+function startDrag(e){
+	var code = '<img id="facepic" src="http://i.stack.imgur.com/Ckrc2.png" height="200px" width="200px">';
+	e.dataTransfer.setData('Text',code);
+
+}
+
+function dropped(e){
+	e.preventDefault();
+	leftbox.innerHTML=e.dataTransfer.getData('Text');	
+}
